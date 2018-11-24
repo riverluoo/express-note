231a1eca20c373f099ce35a772e45a81f1adbce5
wangyangyang <769789170@qq.com>
2018-11-24 22:35:36 +0800

    add post

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 var express= require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.post('/login', urlencodedParser, function (req, res) {
+    res.send('welcome, ' + req.body.username)
+  })
+ 
+  app.post('/api/users', jsonParser, function (req, res) {
+   
+  })
+
+
+app.post("/",function(req,res){
+    console.dir(req.body);
+    res.send(req.body.name);
+})
 
 app.get('/' , function(req,res){
     console.dir(req.query);
